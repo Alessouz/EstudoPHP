16ef4e77e76a2690681af3f0a751442232994f23
Alessouz <93060660+Alessouz@users.noreply.github.com>
2023-12-02 04:07:02 -0300

    Add files via upload

---
+++ api/public/HTML.php
@@ -0,0 +1,6 @@
+<h2 class="titulo">Integrando PHP no HTML</h2>
+
+<?php
+
+
+?>
---
+++ api/public/escritaPHP.php
@@ -0,0 +1,9 @@
+<h2 class="titulo">Escrita no PHP</h2>
+
+<?php
+echo "Existe duas formas de escreve no PHP Web". "<br>";
+echo 'uma usando o echo "..." '. "<br>";
+?>
+
+<?= 'E outra forma de escrever algo com PHP, '?>
+<?= htmlspecialchars('usando o "<?= ... ?>" ') ?>
